2efe696e35e366ddf0a352040ad45a4f318cae21
Teran McKinney <sega01@go-beyond.org>
2018-02-03 19:11:06 +0000

    0.2.2: Require bitcash 0.5.0 or later

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 
 DOWNLOAD_URL = 'https://github.com/teran-mckinney/bitcoinacceptor-python/tarball/{}'
 
@@ -23,7 +23,7 @@ setup(
     ],
     install_requires=[
         'bit',
-        'bitcash',
+        'bitcash>=0.5.0',
         'requests'
     ],
 )
